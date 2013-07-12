2f3c437fd1a382c0638839f1fcc6a7813609e102
Alexander <alexander@aleschenko.ru>
2013-07-12 06:47:49 -0700

    SMTH getting from Kayako %)

---
+++ README.md
@@ -0,0 +1,4 @@
+KayakoNewsToWiki
+================
+
+Check Kayako News periodically for specific news and publish them to Kayako Wiki with fileuploads
---
+++ run.rb
@@ -0,0 +1,35 @@
+require 'kayako_client'
+require 'psych'
+
+configFileName = 'D:\Documents\RubyMine\KayakoNewsToWiki\config.yml'
+
+configFile = begin
+  Psych.load_file(configFileName)
+rescue Psych::SyntaxError => ex
+  puts "Could not parse #{ex.file}: #{ex.message}"
+  exit(1)
+end
+
+KayakoClient::Base.configure do |config|
+  config.api_url    = configFile[:URL]
+  config.api_key    = configFile[:KEY]
+  config.secret_key = configFile[:SECRET]
+end
+
+news = KayakoClient::NewsItem.all()
+
+if news.count > 0
+  news.each do |news|
+    puts "#{news.id}. #{news.subject} :: #{news.contents}"
+  end
+else
+    puts "Notes missing."
+end
+
+stream = Psych::Stream.new(File.open(configFileName, 'w'))
+stream.start
+stream.push({:LastProcessedNewsItem => configFile[:LastProcessedNewsItem],
+             :LastProcessingTime => DateTime.now(), :URL => configFile[:URL],
+             :KEY => configFile[:KEY],
+             :SECRET => configFile[:SECRET]})
+stream.finish
